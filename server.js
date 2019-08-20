3529b1f0f0207628e36e5ab360fed2f670416622
Kanishk Gupta <kanishkgupta2000@gmail.com>
2019-08-21 00:24:21 +0530

    before correct proxy config

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const flash   =require("connect-flash");
+// const flash   =require("connect-flash");
 const connectDB = require("./config/db");
-//const path = require("path");
-const cors = require("cors");
+const path = require("path");
+// const cors = require("cors");
 
 const app = express();
 
@@ -11,7 +11,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.use(cors());
+// app.use(cors());
 
 app.use("/api", require("./routes/contactUs"));
 app.use(function(req,res,next){
@@ -19,6 +19,13 @@ app.use(function(req,res,next){
 	res.locals.success=req.flash("success");
 	next();
 });
+
+if(process.env.NODE_ENV==='production'){
+	//set static folder
+	app.use(express.static('client/build'));
+	app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build',
+	'index.html')));
+}
 
 console.log("hii");
 const PORT = process.env.PORT || 5001;
